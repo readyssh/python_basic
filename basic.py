6661af1cb745047bf7c0d1c07bf7500e0db8c0cc
readyssh <shreyashdeveloper09@gmail.com>
2025-05-13 11:14:55 +0530

    basic if else and userinput

--- basic.py
+++ basic.py
@@ -1,2 +1,19 @@
-print("Hello world")
-print("Hello world")
+# This is a comment in python  ----  
+#print("Hello world")
+
+print("--------------- Let's print your name ------------------")
+
+name = input("Your name please: ")
+
+print("Hi,",name,"lets use some basic stuff of python")
+
+print("--------- The Even odd number game -----------")
+
+num = int(input("Enter a number bewtween 1 to 100 get if it is even or odd :"))
+
+if num % 2 == 0:
+   print("even")
+else:
+   print("odd")
+
+
