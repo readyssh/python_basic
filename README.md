ee20211761d43f3f1369072ebd621addbd35b122
readyssh <shreyashdeveloper09@gmail.com>
2025-05-13 15:53:55 +0530

    list

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## 🔗 Links
 
 [Basic](https://github.com/readyssh/python_basic/blob/master/basic.py)
+[List](https://github.com/readyssh/python_basic/blob/master/list.py)
--- list.py
+++ list.py
@@ -1,5 +1,5 @@
 # --- List in python ---
-
+'''
 l1 =[]
 for i in range(5):
     num = int(input("Enter next no :"))
@@ -14,3 +14,26 @@ print("print using Another for loo")
 for e in l1: 
 #e will have one element at a time
     print(e)
+'''
+
+# given list l1 add 10 to every element
+# store in new list
+# display both  lists
+
+l1=[100,5,7,10,50]
+new_list=[]
+for e in l1:
+    t = e+10
+    new_list.append(t)
+print(l1)
+print(new_list)
+new_list=[]
+for idx in range(len(l1)):
+    t = l1[idx]+10
+    new_list.append(t)
+print(l1)
+print(new_list)
+
+l1=[100,5,7,10,50]
+l2=[e for e in l1]
+print(l2)
