678110a661ce1ea1d89426b92fafb527f2cd2cdf
readyssh <shreyashdeveloper09@gmail.com>
2025-05-15 19:54:28 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# Type of operator 
+
+print(type(0))
+print(type(0.0))
+print(type('A'))
+print(type(print()))
+print(type(3+3j))
+print(type(True))
+
+
+# is instance functions
+
+print(isinstance(10,int))
+print(isinstance(10,float))
+print(isinstance(1,bool))
+print(isinstance(18.8,int))
+print(isinstance(10.8,float))
+print(isinstance(10.8,complex))
+print(isinstance(True,bool))
+print(isinstance(False,bool))
